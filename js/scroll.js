6bdd37fb7baa7f6ab58e32eef99446d6d04eff77
ExEr7um <exer7um@gmail.com>
2018-06-04 11:10:46 +0300

    Scroll to contact us container

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.contact-us').click(function() {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+      $('html, body').animate({
+        scrollTop: $(scroll_el).offset().top
+      }, 500);
+    }
+    return false;
+  });
+});
